dd28d21719e56b53e402f6314c1b4532872e9732
Prabin Dangol <dprabin23@gmail.com>
2025-06-26 12:07:45 +0545

    chore:testimonial issue silve
    Signed-off-by: Prabin Dangol <dprabin23@gmail.com>

--- src/components/TestimonialsPreview.js
+++ src/components/TestimonialsPreview.js
@@ -1,6 +1,7 @@
 import { TESTIMONIALS } from '@/constants/testimonials'
 import TestimonialCard from '@/components/TestimonialCard'
 import Link from 'next/link'
+import { TESTIMONIALS_ROUTE } from '@/constants/routes'
 
 export default function TestimonialsPreview() {
   return (
@@ -9,7 +10,7 @@ export default function TestimonialsPreview() {
         <div className="text-center mb-12">
           <h2 className="text-3xl font-bold text-gray-800 dark:text-white mb-4">Traveler Testimonials</h2>
           <p className="text-gray-600 dark:text-gray-300 max-w-2xl mx-auto">
-            Don't just take my word for it - hear what my clients have to say
+            {`Don't just take my word for it - hear what my clients have to say`}
           </p>
           <div className="w-20 h-1 bg-blue-600 dark:bg-blue-400 mx-auto mt-4"></div>
         </div>
@@ -22,7 +23,7 @@ export default function TestimonialsPreview() {
         
         <div className="text-center">
           <Link
-            href="/testimonials"
+            href={TESTIMONIALS_ROUTE}
             className="inline-block border-2 border-blue-600 text-blue-600 dark:text-blue-400 dark:border-blue-400 hover:bg-blue-50 dark:hover:bg-gray-700 px-6 py-3 rounded-lg transition-colors"
           >
             Read More Reviews
