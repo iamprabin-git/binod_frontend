00accf2570d8fbe1e21bc66669d3d2d86fb3583d
Prabin Dangol <dprabin23@gmail.com>
2025-06-29 19:44:34 +0545

    chore:arrange
    Signed-off-by: Prabin Dangol <dprabin23@gmail.com>

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
+import Header from '@/components/Header'
 import './globals.css'
 import './theme.css'
 import { Inter } from 'next/font/google'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +15,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} theme-transition`}>
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -2,18 +2,15 @@ import Hero from '@/components/Hero'
 import AboutPreview from '@/components/AboutPreview'
 import TourPreview from '@/components/TourPreview'
 import TestimonialsPreview from '@/components/TestimonialsPreview'
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
+
 
 export default function Home() {
   return (
     <main>
-    <Header />
       <Hero />
       <AboutPreview />
       <TourPreview />
       <TestimonialsPreview />
-      <Footer />
     </main>
   )
 }
